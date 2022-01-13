02e319c5c20a1121535a5ba650396a7c34dbd356
Gazuka <jerome.carion@gmail.com>
2022-01-13 11:13:38 +0100

    v1.2.4 - Ajout de DataTables pour les tableaux de l'admin

--- public/template_sb2/js/demo/datatables-demo.js
+++ public/template_sb2/js/demo/datatables-demo.js
@@ -1,4 +1,10 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    language: {
+      // url: "{{ asset('datatables/media/french.json') }}"
+      url: "datatables/media/french.json"
+    }
+  });
 });
+ alert('test');
