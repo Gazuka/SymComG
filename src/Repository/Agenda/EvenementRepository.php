c44b034134c93bb0a2dc07579f67ea4687dea822
Gazuka <jerome.carion@gmail.com>
2022-01-15 13:52:12 +0100

    v1.3.2 - Bug du jour J

--- src/Repository/Agenda/EvenementRepository.php
+++ src/Repository/Agenda/EvenementRepository.php
@@ -4,8 +4,9 @@ namespace App\Repository\Agenda;
 
 use DateTime;
 use App\Entity\Agenda\Evenement;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Validator\Constraints\Date;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @method Evenement|null find($id, $lockMode = null, $lockVersion = null)
@@ -22,8 +23,10 @@ class EvenementRepository extends ServiceEntityRepository
 
     public function findProchains($limit, $offset = 1)
     {
+        $now = new DateTime();
+        $now->setTime(0,0,0,0); // Définir l'heure actuelle a 0h00 afin d'avoir tous les évènements du jour
         return $this->createQueryBuilder('e')
-            ->setParameter('date', new DateTime())
+            ->setParameter('date', $now)
             ->Where('e.date >= :date or e.dateFin > :date')
             ->orderBy('e.date', 'ASC')
             ->setMaxResults($limit)
